ee2eb566d9d4ba15f0f2c996f0655be59f0c8bd0
piabelenr <61482372+piabelenr@users.noreply.github.com>
2020-07-27 22:02:33 -0400

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <Cabecera></Cabecera>
+    
+    
+  </div>
+</template>
+
+<script>
+
+import Cabecera from './components/Cabecera.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Cabecera
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Cabecera.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="container">
+      <h1>Sophie's List of Things to Learn</h1>
+      <ul>
+          <li>HTML </li>
+          <li>CSS </li>
+          <li>Vue </li>
+          <li>Firebase </li>
+      </ul>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'Cabecera'
+}
+</script>
+
+<style>
+
+</style>
